624f3d3530f632087c1f7924aef24f0a527f238a
Hugo Ramos <hu.ramos@duocuc.cl>
2025-03-24 22:41:15 -0300

    "First Commit"

---
+++ src/main/java/com/foro/enums/UserRole.java
@@ -0,0 +1,7 @@
+package com.foro.enums;
+
+public enum UserRole {
+    ADMIN,     // Puede hacer todo
+    MODERATOR, // Puede moderar posts y comentarios, pero no administrar usuarios
+    USER       // Solo puede crear/editar sus propios posts y comentarios
+} 
